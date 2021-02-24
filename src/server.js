99a9b1dc6838482563f1082e9f23efabfa93c183
Artem Valkov <valkov.artyom.fs@gmail.com>
2021-02-24 18:03:11 +0200

    secure

--- src/server.js
+++ src/server.js
@@ -5,13 +5,13 @@ const cors = require('cors');
 const bodyParser = require("body-parser");
 const helmet = require('helmet')
 const jwt = require('jsonwebtoken');
+const expressLogging = require('express-logging');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const socket = require('socket.io')
 
 require('./config/db')
 
-app.disable('x-powered-by')
 app.use(cors())
 app.use(helmet())
 app.use(bodyParser.urlencoded({ extended:true }));
